a3e1e44f113c1e8b6f6a7d8478cf4856a076515c
Rishingn <51052837+Rishingn@users.noreply.github.com>
2020-02-27 10:21:42 +0530

    Add files via upload

---
+++ precache-manifest.60aa460665ea4a635f3118970461ca64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b31515c10dd2b4ad41d22e6411037855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d00126724d248f33688",
+    "url": "/static/css/main.78e06a71.chunk.css"
+  },
+  {
+    "revision": "7c8ce1a394eb683327c0",
+    "url": "/static/js/2.1eacee02.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.1eacee02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d00126724d248f33688",
+    "url": "/static/js/main.e092ffd5.chunk.js"
+  },
+  {
+    "revision": "777cec2ef18f2fee5d2d",
+    "url": "/static/js/runtime-main.11269e21.js"
+  }
+]);
